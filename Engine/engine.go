d33559a848e74b2ef0709db09e4dde777474a0e1
davidleitw <davidleitw@gmail.com>
2022-03-06 02:12:29 +0800

    add engine PrintBoard & randomBoard

--- Engine/engine.go
+++ Engine/engine.go
@@ -1,12 +1,85 @@
 package Engine
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+)
+
+const (
+	BLACKICON = "⬟"
+	BLACKCODE = -1
+	WHITEICON = "⬠"
+	WHITECODE = 1
+)
 
 type GMKEngine struct {
-	boardSize int
+	boardSize  int
+	boardState [][]int
+}
+
+type point struct {
+	x, y int
 }
 
 func NewEngine(boardSize int) *GMKEngine {
 	log.Printf("Create Gomoku engine, board size = %d * %d\n", boardSize, boardSize)
-	return &GMKEngine{boardSize: boardSize}
+	board := &GMKEngine{boardSize: boardSize}
+	board.resetBoard()
+	return board
+}
+
+func (engine *GMKEngine) resetBoard() {
+	board := make([][]int, engine.boardSize)
+	for i := 0; i < engine.boardSize; i++ {
+		board[i] = make([]int, engine.boardSize)
+	}
+	engine.boardState = board
+}
+
+func (engine *GMKEngine) vaildCheckmate() ([]point, bool) {
+	var points []point
+
+	return points, false
+}
+
+// @x,@y: 棋盤座標
+func (engine *GMKEngine) step(x, y int) bool {
+	if x > engine.boardSize || y > engine.boardSize {
+		return false
+	}
+
+	return true
+}
+
+func (engine *GMKEngine) PrintBoard() {
+	engine.randomBoard()
+	board := ""
+	for x := 0; x < engine.boardSize; x++ {
+		for y := 0; y < engine.boardSize; y++ {
+			switch engine.boardState[x][y] {
+			case BLACKCODE:
+				board += BLACKICON
+			case WHITECODE:
+				board += WHITEICON
+			default:
+				board += "."
+			}
+			board += " "
+		}
+		board += "\n"
+	}
+
+	fmt.Println(board)
+}
+
+// To test PrintBoard
+func (engine *GMKEngine) randomBoard() {
+	rand.Seed(time.Now().UnixNano())
+	for x := 0; x < engine.boardSize; x++ {
+		for y := 0; y < engine.boardSize; y++ {
+			engine.boardState[x][y] = -1 + rand.Intn(3)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/davidleitw/Gomoku/Engine"
 )
 
 func main() {
 	engine := Engine.NewEngine(15)
-	fmt.Println(engine)
+	engine.PrintBoard()
 }
